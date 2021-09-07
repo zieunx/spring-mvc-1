064d101d5a203e1b65e303c6ee446aa086819722
zieunx <wldms_1217@naver.com>
2021-09-07 21:56:15 +0900

    response 헤더와 콘텐츠를 세팅하여 응답할 수 있다.

--- servlet/src/main/java/hello/servlet/basic/HelloServlet.java
+++ servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -7,12 +7,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "HelloServlet", urlPatterns = "/hello")
+@WebServlet(name = "HelloServlet", urlPatterns = "/hello") // 중복되지 않도록 주의
 public class HelloServlet extends HttpServlet {
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //        super.service(req, resp);
         System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        // response header setting
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+
+        // response content writing
+        response.getWriter().write("hello " + username);
     }
+
+    /*
+    * 서블릿이 http 요청을 개발자가 직접 파싱하지 않아도 접근할 수 있도록 만들어졌다.
+    * */
 }
