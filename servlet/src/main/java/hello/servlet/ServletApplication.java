575148dd276f24996bc15589a2940c18b731dca3
zieunx <wldms_1217@naver.com>
2021-09-07 21:46:38 +0900

    localhost:8080/hello 를 요청했을 때 메소드가 실행되도록 HttpServlet 을 상속받아서 구현했다.

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+/*
+ * @ServletComponentScan
+ * 스프링이 자동으로 패키지 내의 모든 서블릿을 찾아서 자동으로 등록해준다.
+ * */
 @ServletComponentScan // 서블릿 자동 등록
 @SpringBootApplication
 public class ServletApplication {
